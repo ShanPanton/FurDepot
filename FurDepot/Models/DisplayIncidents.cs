67a52b40b55913a353a6fe35dcdd6cd3234967ca
msoph <43834420+msoph@users.noreply.github.com>
2021-11-22 19:36:41 -0500

    Mai 11.22

---
+++ FurDepot/Models/DisplayIncidents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FurDepot.Models
+{
+	public class DisplayIncidents
+	{
+		public string IncidentID { get; set; }
+		public string OwnerUserName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string IncidentTitle { get; set; }
+		public string IncidentDesc { get; set; }
+		public string UserName { get; set; }
+		public string SubmittedDate { get; set; }
+		public string Resolved { get; set; }
+
+		public List<DisplayIncidents> incidentinfo { get; set; }
+	}
+
+}
